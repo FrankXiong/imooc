56f004da156cf4d18c3af3d140f697ae417477a6
FrankXiong <1131076966@qq.com>
2016-04-01 17:51:48 +0800

    同步豆瓣数据

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -8,7 +8,7 @@ exports.index = function(req,res){
     Category
         .find({})
         // 限制每个分类下只显示5条数据
-        .populate({path:'movies',options:{limit:5}})
+        .populate({path:'movies',options:{limit:4}})
         .exec(function(err,categories){
             if(err){
                 console.log(err)
--- dist/static/js/admin.js
+++ dist/static/js/admin.js
@@ -17,25 +17,63 @@ $(function(){
             }
         })
     })
-});
 
-$(function(){
-    $('.del').click(function(e){
-        var target = $(e.target)
-        var id = target.data('id')
-        var tr = $('.item-id-' + id)
+    $('#inputDouban').blur(function(){
+        var douban = $(this)
+        var id = douban.val()
 
-        $.ajax({
-            type:'DELETE',
-            url:'/admin/user/list?id=' + id
-        })
-        .done(function(results){
-            if(results.success === 1){
-                if(tr.length > 0){
-                    tr.remove()
-                    alert('成功删除')
-                }
-            }
-        })
+        if(id){
+           $.ajax({
+                url:'https://api.douban.com/v2/movie/subject/' + id,
+                type:'GET',
+                dataType:'jsonp',
+                crossDomain:true,
+                cache:true,
+                jsonp:'callback',
+                success:function(data){
+                    $('#inputTitle').val(data.title)
+                    $('#inputCountry').val(data.countries[0])
+                    $('#inputDirector').val(data.directors[0].name)
+                    // $('#inputLanguage').val(languages[0])
+                    $('#inputYear').val(data.year)
+                    $('#inputPoster').val(data.images.large)
+                    // $('#inputFlash').val(data.trailer_urls[0])
+                    $('#inputSummary').val(data.summary)               
+                }   
+            }) 
+        }
+        
     })
 });
+
+// "directors": [{
+//     "alt": "https://movie.douban.com/celebrity/1031904/", 
+//     "avatars": {
+//         "small": "https://img3.doubanio.com/img/celebrity/small/23346.jpg", 
+//         "large": "https://img3.doubanio.com/img/celebrity/large/23346.jpg", 
+//         "medium": "https://img3.doubanio.com/img/celebrity/medium/23346.jpg"
+//     }, 
+//     "name": "扎克·施奈德", 
+//     "id": "1031904"
+// }]
+
+// $(function(){
+//     $('.del').click(function(e){
+//         var target = $(e.target)
+//         var id = target.data('id')
+//         var tr = $('.item-id-' + id)
+
+//         $.ajax({
+//             type:'DELETE',
+//             url:'/admin/user/list?id=' + id
+//         })
+//         .done(function(results){
+//             if(results.success === 1){
+//                 if(tr.length > 0){
+//                     tr.remove()
+//                     alert('成功删除')
+//                 }
+//             }
+//         })
+//     })
+// });
